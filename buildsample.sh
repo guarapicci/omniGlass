f722577c728e77f2bc0eb261e76c5a415b028d3e
GuaraPicci <none>
2023-10-25 01:43:36 -0300

    smarter build script

--- buildsample.sh
+++ buildsample.sh
@@ -1,4 +1,6 @@
 #!/bin/sh
+
+[ -e build ] || mkdir build
 cp src/omniglass_linux.lua build/ &&\
 gcc -fPIC -shared src/platform_linux.c src/omniglass.c -llua5.1 -levdev -o build/libomniglass.so &&\
 gcc src/tests/init.c -L./build -lomniglass -o build/test.elf &&\
