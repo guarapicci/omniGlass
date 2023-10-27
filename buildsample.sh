80a91c1bf43b4b602d15e4b41313a4facee1d45d
GuaraPicci <none>
2023-10-27 02:07:23 -0300

    update simple build script

--- buildsample.sh
+++ buildsample.sh
@@ -6,6 +6,7 @@
 # build the library using gcc
 [ -e build ] || mkdir build
 cp src/omniglass_linux.lua build/ &&\
+cp templates/config.lua build/ &&\
 gcc -fPIC -shared src/platform_linux.c src/omniglass.c -I/usr/include/lua5.1 -L/usr/lib/lua5.1 -llua5.1 -I/usr/include/libevdev-1.0 -levdev -o build/libomniglass.so &&\
 gcc src/tests/init.c -L./build -lomniglass -o build/test.elf &&\
 cd build &&\
