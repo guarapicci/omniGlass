d9b4ab01e1cd0f971cd2915a6ee31040bbe5c2f2
GuaraPicci <none>
2023-10-28 23:43:20 -0300

    added some simple functions to automate building

---
+++ utils.sh
@@ -0,0 +1,15 @@
+export OMNIGLASS_PROJECT_ROOT="$(pwd)"
+export OMNIGLASS_BUILD_DIR="$OMNIGLASS_PROJECT_ROOT/output"
+
+function cmgen() {
+    cmake -S "$OMNIGLASS_PROJECT_ROOT" -B "$OMNIGLASS_BUILD_DIR" &&\
+    export OMNIGLASS_BUILDER_WAS_GENERATED=true;
+    cd "$OMNIGLASS_PROJECT_ROOT"
+}
+
+function rebuild() {
+    { [ -v OMNIGLASS_BUILDER_WAS_GENERATED ] || cmgen; } &&\
+    cd "$OMNIGLASS_BUILD_DIR" &&\
+    make;
+    cd "$OMNIGLASS_PROJECT_ROOT"
+}
