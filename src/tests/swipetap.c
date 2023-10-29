d586be8224cefb71541e85a8c869662de6c9ba3b
GuaraPicci <none>
2023-10-29 13:48:39 -0300

    swipe-n-tap gesture test

---
+++ src/tests/swipetap.c
@@ -0,0 +1,29 @@
+#include "../omniglass.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <time.h>
+
+void on_slide(double value) {
+    if (value < 0)
+        printf("moved left\t\t%g\n", value);
+    else if (value > 0)
+        printf("moved right\t\t%g\n",value);
+}
+
+int main(int argc, char **argv) {
+        
+    struct omniglass *handle;
+    if (omniglass_init(&handle) != OMNIGLASS_RESULT_SUCCESS)
+        fprintf(stderr,"could not initialize omniglass.\n");
+    omniglass_listen_gesture_slide(handle,&on_slide);
+    
+    //8 milliseconds polling rate for touchpad events
+    struct timespec poll_rate = {0, 8000000};
+    
+    while (true) {
+        omniglass_step(handle);
+    }
+    
+    return 0;
+}
