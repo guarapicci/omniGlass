4904960a020c4a6265990905a9caf2f4633c4600
GuaraPicci <none>
2024-03-06 01:21:59 -0300

    new "edge_swipe_multi" executable, detects edge swipe for all 4 edges

---
+++ src/tests/edge_swipe_multi.c
@@ -0,0 +1,47 @@
+#include "../omniglass.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+
+void edge_slide_report(double value, char *flavour_text) {
+    if (value < 0)
+        printf("moved on %s edge, negative\t\t%g\n", flavour_text, value);
+    else if (value > 0)
+        printf("moved on %s edge, positive\t\t%g\n", flavour_text, value);
+    fflush(stdout);
+}
+
+void on_edge_slide_left(double value){
+    edge_slide_report(value, "left");
+}
+void on_edge_slide_bottom(double value) {
+    edge_slide_report(value, "bottom");
+}
+void on_edge_slide_top(double value) {
+    edge_slide_report(value, "top");
+}
+void on_edge_slide_right(double value) {
+    edge_slide_report(value, "right");
+}
+int main(int argc, char **argv) {
+        
+    struct omniglass *handle;
+    if (omniglass_init(&handle) != OMNIGLASS_RESULT_SUCCESS)
+        fprintf(stderr,"could not initialize omniglass.\n");
+    omniglass_listen_gesture_edge_left(handle, on_edge_slide_left);
+    omniglass_listen_gesture_edge_bottom(handle, on_edge_slide_bottom);
+    omniglass_listen_gesture_edge_top(handle, on_edge_slide_top);
+    omniglass_listen_gesture_edge_right(handle, on_edge_slide_right);
+    fflush(stdout);
+
+    printf("starting event loop\n");
+    while (true) {
+        usleep(1000);
+        omniglass_step(handle);
+        fflush(stdout);
+    }
+    
+    return 0;
+}
