0475ca93d5581f5adc40f1130b1a63c1372c2470
NetYai <NetYai@nowhere.none>
2024-04-10 18:48:16 -0300

    Added touches-changed callback test
    this simple program registers for the touches-changed callback
    from libomniglass and dumps all touches when bodies
    start or end contact with the surface.

---
+++ src/tests/touching.c
@@ -0,0 +1,49 @@
+#include "../omniglass.h"
+#include <stdio.h>
+#include <stdlib.h>
+#include <stdbool.h>
+#include <unistd.h>
+
+
+struct state {
+    int elapsed;
+    int stars;
+};
+
+
+void on_touches_changed(omniglass_raw_report *report, void *passthrough){
+    struct state *ext_state = passthrough;
+    ext_state->elapsed++;
+    printf("(%d) touch change detected. touches are at :", (ext_state->elapsed));
+    for (int i = 0; i < report->points_max; i++){
+        omniglass_raw_touchpoint *current_point = &(report->points[i]);
+        if (current_point->is_touching)
+            printf("(%f,%f)",current_point->x, current_point->y);
+        else
+            printf("(no touch)");
+        if (i < (report->points_max - 1))
+            printf(",");
+    }
+    printf("\n");
+}
+
+int main(int argc, char **argv) {
+    struct omniglass *handle;
+    struct state top_state = {
+        0,
+        0
+    };
+    if (omniglass_init(&handle) != OMNIGLASS_RESULT_SUCCESS)
+        fprintf(stderr,"could not initialize omniglass.\n");
+    omniglass_listen_gesture_touches_changed(handle, on_touches_changed, &top_state);
+    fflush(stdout);
+
+    printf("starting event loop\n");
+    while (true) {
+        usleep(1000);
+        omniglass_step(handle);
+        fflush(stdout);
+    }
+    
+    return 0;
+}
