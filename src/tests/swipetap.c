2f3e8efcc8d7e4ccf87a9a20a1974b9b5f215d87
NetYai <NetYai@nowhere.none>
2024-04-10 23:56:34 -0300

    updated swipetap test to use slide gesture passthrough

--- src/tests/swipetap.c
+++ src/tests/swipetap.c
@@ -4,7 +4,13 @@
 #include <stdbool.h>
 #include <time.h>
 
-void on_slide(double x, double y) {
+typedef struct app_data {
+        int counter;
+        char *message;
+    } app_data;
+
+void on_slide(double x, double y, void *passthrough) {
+    app_data *passed_data = passthrough;
     if (x < 0)
         printf("moved left\t%g", x);
     else if (x > 0)
@@ -14,15 +20,23 @@ void on_slide(double x, double y) {
     else if (y < 0)
         printf(",\tmoved up\t%g",y);
     printf("\n");
+    passed_data->counter++;
+    if ( (passed_data->counter) % 10 == 0)
+        printf("%s\n", passed_data->message);
     fflush(stdout);
 }
 
 int main(int argc, char **argv) {
+
+    app_data somedata = {
+        0,
+        "10 reports have passed."
+    };
         
     struct omniglass *handle;
     if (omniglass_init(&handle) != OMNIGLASS_RESULT_SUCCESS)
         fprintf(stderr,"could not initialize omniglass.\n");
-    omniglass_listen_gesture_slide(handle,&on_slide);
+    omniglass_listen_gesture_slide(handle,&on_slide, &somedata);
     fflush(stdout);
     //8 milliseconds polling rate for touchpad events
     struct timespec poll_rate = {0, 8000000};
