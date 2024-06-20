9153513c338686a82459c5f06424bb06eabf80c9
Guarapicci <guarapicci@none>
2024-06-20 16:35:05 -0300

    config file self-documents.

--- templates/config.lua
+++ templates/config.lua
@@ -1,18 +1,24 @@
 -- (required) add your touchpad device file path here 
 return {
-    --path to the touchpad device file (usually at /dev/input or /dev/input/by-path)
-    touchpad_file_path = "/dev/input/event81",
+    --Path to the primary touchpad device file (usually at /dev/input or /dev/input/by-path).
+    -- This device will be prioritized over all others.
+    touchpad_file_path = "",
 
-    --if the selected touchpad is not available,
-    -- try scanning the available devices for anything that looks like a touchpad
-    scan_for_touchpads = true,
+    --If the selected touchpad is not available,
+    -- try scanning the available devices for anything that looks like a touchpad.
+    scan_for_touchpads = false,
     
-    --how big the edges of the screen are.
-    --0.0 is no edge; 0.33 means up to a third of the distance to the opposite edge; 1.0 means all the touchpad is edges (recommended not to go past 0.45)
+    --How big the edges of the screen are.
+    -- 0.0 is no edge;
+    -- 0.33 means up to a third of the distance to the opposite edge;
+    -- 1.0 means all the touchpad is all the edges simultaneously (recommended not to go past 0.45)
     edge_width = 0.1,
 
-    --ratio of device units to real-world millimeters.
-    --This may vary from device to device.
+    --Ratio of real-world millimeters per device unit.
+    -- (scale = width/(right_edge_X - left_edge_X))
+    -- This is NOT a sensitivity value. A scale too low or too high might break gestures.
+    -- In future revisions this value may be auto-detected from HID descriptors.
+    -- This may vary from device to device.
     scale = 0.028,
     
     --axis inversion.
